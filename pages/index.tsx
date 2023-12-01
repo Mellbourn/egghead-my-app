1ca7115827c42684f245f53493087fcc6a775964
Klas Mellbourn <klas@mellbourn.net>
2023-12-01 20:53:11 +0100

    more style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import { NextPage } from "next";
-import Link from "next/link";
+import NextLink from "next/link";
+import { Link } from "@chakra-ui/react";
 
 const IndexPage: NextPage = () => {
   return (
     <>
       <h1>Hello World</h1>
-      <Link href="/about">About</Link>
+      <NextLink href="/about">
+        <Link>About</Link>
+      </NextLink>
     </>
   );
 };
