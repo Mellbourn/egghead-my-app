355bd24b42e52bc8b3d01ee1ff45e3a4c7813c88
Klas Mellbourn <klas@mellbourn.net>
2023-12-01 20:12:08 +0100

    navigation between pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,13 @@
 import { NextPage } from "next";
+import Link from "next/link";
 
 const AboutPage: NextPage = () => {
-  return <h1>This is ABOUT page</h1>;
+  return (
+    <>
+      <h1>This is ABOUT page</h1>
+      <Link href="/">Home</Link>
+    </>
+  );
 };
 
 export default AboutPage;
