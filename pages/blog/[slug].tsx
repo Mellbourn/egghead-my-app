4f0c3a1077561694a01051cd94275da82a6ad2b0
Klas Mellbourn <klas@mellbourn.net>
2023-12-08 15:15:11 +0100

    get statix content for slugs

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,16 +1,45 @@
-import { Text, Flex, Heading } from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import NextLink from "next/link";
+import { Text, Flex, Heading, Link } from "@chakra-ui/react";
+import { GetStaticProps, GetStaticPaths, NextPage } from "next";
 
-const BlogPage = () => {
-  const router = useRouter();
-  const { slug } = router.query;
+const BlogPage: NextPage<{
+  title: string;
+  text: string;
+}> = (props) => {
   return (
-    <Flex margin={4}>
-      <Heading as="h1" size="lg">
-        Slug is "{slug}"
+    <Flex margin={4} flexDirection="column">
+      <NextLink href="/blog" passHref>
+        <Link>Back to TOC</Link>
+      </NextLink>
+      <Heading as="h1" size="xl" marginY={4}>
+        {props.title}
       </Heading>
+      <Text>{props.text}</Text>
     </Flex>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const blogs = (await import("../../lib/blogs.json")).default;
+  const slugs = blogs.map((blog) => blog.slug);
+  const paths = slugs.map((slug) => ({ params: { slug } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params: { slug } }) => {
+  const blogs = (await import("../../lib/blogs.json")).default;
+  const blog = blogs.find((x) => x.slug === slug);
+
+  return {
+    props: {
+      title: blog.title,
+      text: blog.text,
+    },
+  };
 };
 
 export default BlogPage;
