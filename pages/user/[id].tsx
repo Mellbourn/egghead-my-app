d65c514bc38dbef620d98a426d8e9b5c969bd27a
Klas Mellbourn <klas@mellbourn.net>
2023-12-08 13:29:44 +0100

    use api to get user

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,14 +1,69 @@
+import { Alert, Box, Flex, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { useRouter } from "next/router";
+import useSWR from "swr";
+type Data = {
+  id: string;
+  name: string;
+  email: string;
+};
 
-const User = () => {
+const fetcher = async (url: string) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw Error("Yo that's NOT OK!!!");
+  }
+  const data: Data = await res.json();
+  return data;
+};
+
+const UserData = () => {
   const router = useRouter();
-  const { id, ...rest } = router.query;
+  const { id } = router.query;
+  const result = useSWR(`/api/user/${id}`, fetcher);
+  const data: Data = result.data;
+  const error: Error = result.error;
+
+  if (error) {
+    return <Alert status="error">Loading failed: {error.message}</Alert>;
+  }
+
+  if (!data) {
+    return <Alert status="info">Loading...</Alert>;
+  }
+
   return (
-    <>
-      <h2>User ID={id}</h2>
-      <pre>{JSON.stringify(rest)}</pre>
-    </>
+    <SimpleGrid columns={2} width="2xs" spacingY={4}>
+      <Text fontWeight="bold" marginRight={4}>
+        UserID
+      </Text>
+      <Text>{data.id}</Text>
+
+      <Text fontWeight="bold" marginRight={4}>
+        Name
+      </Text>
+      <Text>{data.name}</Text>
+
+      <Text fontWeight="bold" marginRight={4}>
+        Email
+      </Text>
+      <Text>{data.email}</Text>
+    </SimpleGrid>
   );
 };
 
-export default User;
+const UserPage = () => {
+  return (
+    <Box>
+      <Flex flexDirection="column" alignItems="center">
+        <Heading marginY="2rem">User</Heading>
+        <UserData />
+        <NextLink href="/">
+          <Text fontStyle="italic">Go back home</Text>
+        </NextLink>
+      </Flex>
+    </Box>
+  );
+};
+
+export default UserPage;
